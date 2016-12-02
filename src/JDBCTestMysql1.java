bb5cb2d9d9f1e299902fae8aba62d164e70965c7
Abby Schmiedt <abbyschmiedt@pcp066743pcs.wireless.calpoly.edu>
2016-12-02 14:50:27 -0800

    added calculate average for a team

--- src/JDBCTestMysql1.java
+++ src/JDBCTestMysql1.java
@@ -5,6 +5,7 @@ import java.io.FileReader;
 // -----------------------
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
@@ -16,16 +17,52 @@ public class JDBCTestMysql1 {
 //////////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////CREATE CONNECTION///////////////////////
 
-    public static String findTeamAverage(String teamName){
+    
+    
+    public static Double findTeamAverage(String teamId){
+		Integer homeCount = 0;
+		Integer homeScore = 0;
+		Integer awayCount = 0;
+		Integer awayScore = 0;
+
     	try {
 			Statement s1 = conn.createStatement();
-	        String table = "SELECT AVG( FROM " + teamName + " ";
-	        table = table + "(LibCode INT, Title VARCHAR(50), Author VARCHAR (50),";
-	        table = table + "PRIMARY KEY (LibCode) )";
+	        String homeQuery = "SELECT count(m.Id) as count, SUM(m.HomeScore) as Avg ";
+	        homeQuery = homeQuery + "FROM Team t, Matches m ";
+	        homeQuery = homeQuery + "WHERE t.TeamId = m.HomeTeamId"
+	        		      + " and t.TeamId = " + teamId 
+	        		      + " Group by m.HomeTeamId";
+	        ResultSet result = s1.executeQuery(homeQuery);
+	        boolean f = result.next(); 
+            while (f)
+               {
+                homeCount = Integer.parseInt(result.getString(1));
+                homeScore = Integer.parseInt(result.getString(2));
+                f=result.next();
+               }
+
+	        String awayQuery = "SELECT count(m.Id) as count, SUM(m.AwayScore) as Avg ";
+	        awayQuery = awayQuery + "FROM Team t, Matches m ";
+	        awayQuery = awayQuery + "WHERE t.TeamId = m.AwayTeamId"
+	        		      + " and t.TeamId = " + teamId 
+	        		      + " Group by m.AwayTeamId";
+	        
+	        ResultSet awayResults = s1.executeQuery(awayQuery);
+	        boolean away = awayResults.next(); 
+            while (away)
+               {
+                awayCount = Integer.parseInt(awayResults.getString(1));
+                awayScore = Integer.parseInt(awayResults.getString(2));
+                away=awayResults.next();
+               }
+
+            
+
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
-        return "";
+    	System.out.println(homeScore + ", " + awayScore + ", " + (homeCount) + ", " + awayCount);
+        return (double) (homeScore + awayScore / (homeCount + awayCount));
 
     }
     
@@ -55,6 +92,7 @@ public class JDBCTestMysql1 {
         };
        
       System.out.println("Connected");
+
       try {
     	  ScriptRunner runner = new ScriptRunner(conn, false, true);
     	  runner.runScript(new BufferedReader(new FileReader("setup.sql")));
@@ -68,7 +106,6 @@ public class JDBCTestMysql1 {
      	  runner.runScript(new BufferedReader(new FileReader("build-League.sql")));
      	  runner.runScript(new BufferedReader(new FileReader("build-Team.sql")));
      	  runner.runScript(new BufferedReader(new FileReader("build-Matches.sql")));
-
            }  catch (Exception ee) {System.out.println(ee);}        
 
 
