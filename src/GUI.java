f167d05311387dd9f280412090520c2b84a8479f
jacobrickman <jacobmrickman@gmailcom>
2016-12-09 10:19:28 -0800

    Added an actionHandler so that the connection will close on exiting the application

--- src/GUI.java
+++ src/GUI.java
@@ -12,6 +12,8 @@ import java.awt.Font;
 import java.awt.Insets;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
 public class GUI {
 	private static JTable table;
@@ -36,6 +38,12 @@ public class GUI {
 
         //Create and set up the window.
         JFrame frame = new JFrame("KickIt");
+        frame.addWindowListener(new WindowAdapter() {
+        	@Override
+        	public void windowClosing(WindowEvent e) {
+        		Result.closeConnection();
+        	}
+        });
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(800, 800);
         
